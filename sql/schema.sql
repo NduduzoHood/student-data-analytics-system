94c00f49be5d11326cf5748e26335fbd40d579d1
Nduduzo Ngubane <nduduzongubs@gmail.com>
2026-05-01 19:51:16 +0200

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,1 +1,18 @@
+CREATE TABLE students (
+  id INT PRIMARY KEY,
+  name VARCHAR(100)
+);
 
+CREATE TABLE modules (
+  id INT PRIMARY KEY,
+  module_name VARCHAR(100)
+);
+
+CREATE TABLE marks (
+  id INT PRIMARY KEY,
+  student_id INT,
+  module_id INT,
+  test1 INT,
+  test2 INT,
+  exam INT
+);
